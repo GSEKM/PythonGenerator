df8647c5fa6adca8039086046cfb0179beed2962
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-07-10 16:48:44 -0300

    digitalPorts count

--- gen.cpp
+++ gen.cpp
@@ -1,5 +1,14 @@
-// Code generated for Arduino UNO
-// with 24 digital ports in total
-// and  5 analog ports in total
 //@Motor.1 //@Motor.0 //@Motor.2
-motorLib
+#include <libServo>
+servo motor1 = servo(0)
+servo motor2 = servo(0)
+#include <libStepper>
+stepper motor3 = stepper(0)
+stepper motor4 = stepper(0)
+stepper motor5 = stepper(0)
+// Code generated for Arduino UNO
+// with 24 digital ports in total with 5 in use and 19 free
+// and  5 analog ports in total
+void setup(){
+
+}
--- generator.py
+++ generator.py
@@ -19,19 +19,37 @@ with open('gen.cpp', 'a') as file:
     # p(root.tag)
     libs = []
 
+
     for i, component in enumerate(root):
         # p(component.tag)#, component.attrib)
         if component.tag =='Arduino':
+            arduinoModel = component.get('model')
+            totalDigitalPorts = int(component.get('digitalPorts'))
+            totalAnalogPorts = component.get('analogPorts')
+            p(component.get('connector'))
             # p(component.text)
             # p("found arduino!")
-            p('// Code generated for Arduino ',component.get('model'))
-            p('// with ',component.get('digitalPorts'),' digital ports in total')
-            p('// and  ',component.get('analogPorts'),' analog ports in total')
-            p(component.get('conector'))
-        
+            digitalPorts=0
+            analogPorts=0
 
         else:
             lib = component.get('library')
             if lib not in libs:
                 libs.append(lib)
-                p(lib)
+                p('#include <',lib,'>')
+            
+
+
+            for port in range(0,int(component.get('digitalPorts'))):
+                digitalPorts+=1
+                p(component.get('type'),' ',component.get('name'),' = ', component.get('type'),'(',  port,')')
+                
+
+
+
+    p('// Code generated for Arduino ', arduinoModel)
+    p('// with ',totalDigitalPorts,' digital ports in total with ',digitalPorts,' in use and ',totalDigitalPorts-digitalPorts,' free')
+    p('// and  ',totalAnalogPorts,' analog ports in total')
+
+    p('void setup(){\n\n}')
+    p('void loop(){\n\n}')

06bc2bf8f3d8f8f5042a01729bb3a9c6bb95cf94
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2019-09-20 08:46:46 -0300

    WIP

---
+++ UMLet/gen.cpp
@@ -0,0 +1,9 @@
+// Code generated for Arduino UNO
+// with 24 digital ports in total with 200 in use and 200 free
+// and  5 analog ports in total
+void setup(){
+
+}
+void loop(){
+
+}
--- UMLet/umlet.py
+++ UMLet/umlet.py
@@ -1,14 +1,13 @@
 import unicodedata
-
 import xml.etree.ElementTree as ET
-
 libs = []
-
 components = []
 
 
 class Component:
-    def __init__(self, digitalPorts, analogPorts, libName, coordinates, group):
+    def __init__(self, name, model, digitalPorts, analogPorts, libName, coordinates, group):
+        self.name = name
+        self.model = model
         self.digitalPorts = digitalPorts
         self.analogPorts = analogPorts
         self.libName = libName
@@ -42,7 +41,8 @@ relations = []
 
 
 class Relation:
-    def __init__(self, fromEle, toEle, additional, coordinates):
+    def __init__(self, name, fromEle, toEle, additional, coordinates):
+        self.name = name
         self.fromEle = fromEle
         self.toEle = toEle
         self.additional = additional
@@ -54,137 +54,134 @@ def readUML():
     tree = ET.parse('./UMLet/Model2.xml')
     root = tree.getroot()
 
-    file = open('gen.cpp', 'w')  # clear file
-    with open('gen.cpp', 'a') as file:
+    # p(root.tag)
 
-        def p(*args, **kwargs):
-            print(''.join(map(str, args)), **kwargs)
-            file.write(''.join(map(str, args)), **kwargs)
-            file.write('\n')
+    for i, element in enumerate(root):
 
-        file.truncate()
-        # p(root.tag)
+        if element.tag == 'element':
+            # p(component.tag)#, component.attrib)
+            coordinates = {}
+            coordinates['x'] = element[1][0].text
+            coordinates['y'] = element[1][1].text
+            coordinates['w'] = element[1][2].text
+            coordinates['h'] = element[1][3].text
 
-        for i, element in enumerate(root):
+            attributes = element[2].text
+            digitalPorts = 0
+            analogPorts = 0
+            method = 'none'
+            group = 0
+            lib = 'none'
+            model = 'none'
+            for line in attributes.splitlines():
+                if 'digitalPorts' in line:
+                    digitalPorts = int(''.join(filter(str.isdigit, line)))
+                    # print('found digital: ', digitalPorts)
+                elif 'analogPorts' in line:
+                    analogPorts = int(''.join(filter(str.isdigit, line)))
+                elif 'group' in line:
+                    group = int(''.join(filter(str.isdigit, line)))
+                    # print('found group: ', group)
+                elif ')' in line:
+                    methodText = line
+                elif 'lib' in line:
+                    lib = line.replace('lib', '').replace(
+                        ' ', '').replace('=', '')
+                elif 'model' in line:
+                    model = lib = line.replace('model', '').replace(
+                        ' ', '').replace('=', '')
 
-            if element.tag == 'element':
-                # p(component.tag)#, component.attrib)
-                coordinates = {}
-                coordinates['x'] = element[1][0].text
-                coordinates['y'] = element[1][1].text
-                coordinates['w'] = element[1][2].text
-                coordinates['h'] = element[1][3].text
+            name = attributes.splitlines()[0]
+            if 'method' in attributes:
+                global methods
+                # tempElement = {}
+                # tempElement = Method
+                # tempElement.text = methodText
+                # tempElement.coordinates = coordinates
+                # tempElement.group = group
+                # group = 0
+                methods.append(Method(methodText, group, coordinates))
+                # methods[len(methods):] = [tempElement]
+                # print('tempmethodgroup', methods[-1].group)
+                # method
 
-                attributes = element[2].text
-                digitalPorts = 0
-                analogPorts = 0
-                method = 'none'
-                group = 0
-                lib = 'none'
+            elif 'Arduino' in attributes:
+                # print('found')
+                global arduino
+                arduino.name = name
+                arduino.model = model
+                arduino.digitalPorts = digitalPorts
+                arduino.analogPorts = analogPorts
+                arduino.coordinates = coordinates
+                arduino.group = group
 
-                for line in attributes.splitlines():
-                    if 'digitalPorts' in line:
-                        digitalPorts = int(''.join(filter(str.isdigit, line)))
-                        # print('found digital: ', digitalPorts)
-                    elif 'analogPorts' in line:
-                        analogPorts = int(''.join(filter(str.isdigit, line)))
-                    elif 'group' in line:
-                        group = int(''.join(filter(str.isdigit, line)))
-                        # print('found group: ', group)
-                    elif ')' in line:
-                        methodText = line
-                    elif 'lib' in line:
-                        lib = line.replace('lib', '').replace(
-                            ' ', '').replace('=', '')
+            elif element[0].text == "Relation":
+                global relations
+                # tempElement = {}
+                # tempElement = Relation
+                # tempElement.additional = element[3].text
+                # tempElement.coordinates = coordinates
+                # tempElement.group = group
 
-                if 'method' in attributes:
-                    global methods
-                    # tempElement = {}
-                    # tempElement = Method
-                    # tempElement.text = methodText
-                    # tempElement.coordinates = coordinates
-                    # tempElement.group = group
-                    # group = 0
-                    methods.append(Method(methodText, group, coordinates))
-                    # methods[len(methods):] = [tempElement]
-                    # print('tempmethodgroup', methods[-1].group)
-                    # method
-
-                elif 'Arduino' in attributes:
-                    # print('found')
-                    global arduino
-                    arduino.digitalPorts = digitalPorts
-                    arduino.analogPorts = analogPorts
-                    arduino.coordinates = coordinates
-                    arduino.group = group
-
-                elif element[0].text == "Relation":
-                    global relations
-                    # tempElement = {}
-                    # tempElement = Relation
-                    # tempElement.additional = element[3].text
-                    # tempElement.coordinates = coordinates
-                    # tempElement.group = group
-
-                    relations.append(
-                        Relation('', '', element[3].text, coordinates))
-                else:
-                    global components
-                    # tempElement = {}
-                    # tempElement = Component
-                    # tempElement.digitalPorts = digitalPorts
-                    # # print('here digital', tempElement.digitalPorts)
-                    # tempElement.analogPorts = analogPorts
-                    # tempElement.coordinates = coordinates
-                    # tempElement.group = group
-                    components.append(
-                        Component(digitalPorts, analogPorts, lib, coordinates, group))
+                relations.append(
+                    Relation(name, '', '', element[3].text, coordinates))
+            else:
+                global components
+                # tempElement = {}
+                # tempElement = Component
+                # tempElement.digitalPorts = digitalPorts
+                # # print('here digital', tempElement.digitalPorts)
+                # tempElement.analogPorts = analogPorts
+                # tempElement.coordinates = coordinates
+                # tempElement.group = group
+                components.append(
+                    Component(name, model, digitalPorts, analogPorts, lib, coordinates, group))
         # for method in methods:
-            # print('methodgroup', method.group)
-            # if element[0].text == 'UMLClass':
-            #     # for child in element:
-            #     #     if child.tag == 'coordinates':
-            #     #         print(child[0].text)
+        # print('methodgroup', method.group)
+        # if element[0].text == 'UMLClass':
+        #     # for child in element:
+        #     #     if child.tag == 'coordinates':
+        #     #         print(child[0].text)
 
-            # if child.tag == 'id':
-            #     if child.text == 'Relation':
+        # if child.tag == 'id':
+        #     if child.text == 'Relation':
 
-            #         Relation rel
-            # print(child)
-            # print(coordinate.tag)
-            # elif child.tag == 'panel_attributes':
-            #     print(child.tag)
+        #         Relation rel
+        # print(child)
+        # print(coordinate.tag)
+        # elif child.tag == 'panel_attributes':
+        #     print(child.tag)
 
-            # arduinoModel = element.get('model')
-            # totalDigitalPorts = int(element.get('digitalPorts'))
-            # totalAnalogPorts = element.get('analogPorts')
-            # p(child.get('connector'))
-            # p(component.text)
-            # digitalPorts = 0
-            # analogPorts = 0
+        # arduinoModel = element.get('model')
+        # totalDigitalPorts = int(element.get('digitalPorts'))
+        # totalAnalogPorts = element.get('analogPorts')
+        # p(child.get('connector'))
+        # p(component.text)
+        # digitalPorts = 0
+        # analogPorts = 0
 
-            # else:
-            #     libName = element.get('library')
-            #     libPath = 'arduino-libraries/'+libName+'/src/'+libName+'.h'
-            #     libFile = open('arduino-libraries/'+libName +
-            #                    '/src/'+libName+'.h', 'r')
-            #     if libName not in libs:
-            #         libs.append(libName)
-            #         p('#include <'+libPath+'>')
-            #         # p('#include <',libName,'>')
+        # else:
+        #     libName = element.get('library')
+        #     libPath = 'arduino-libraries/'+libName+'/src/'+libName+'.h'
+        #     libFile = open('arduino-libraries/'+libName +
+        #                    '/src/'+libName+'.h', 'r')
+        #     if libName not in libs:
+        #         libs.append(libName)
+        #         p('#include <'+libPath+'>')
+        #         # p('#include <',libName,'>')
 
-            #     for port in range(0, int(element.get('digitalPorts'))):
-            #         digitalPorts += 1
-            #         p(element.get('type'), ' ', element.get('name'),
-            #           ' = ', element.get('type'), '(',  digitalPorts, ')')
+        #     for port in range(0, int(element.get('digitalPorts'))):
+        #         digitalPorts += 1
+        #         p(element.get('type'), ' ', element.get('name'),
+        #           ' = ', element.get('type'), '(',  digitalPorts, ')')
 
-            # p('// Code generated for Arduino ', arduinoModel)
-            # p('// with ', totalDigitalPorts, ' digital ports in total with ',
-            #   digitalPorts, ' in use and ', totalDigitalPorts-digitalPorts, ' free')
-            # p('// and  ', totalAnalogPorts, ' analog ports in total')
+        # p('// Code generated for Arduino ', arduinoModel)
+        # p('// with ', totalDigitalPorts, ' digital ports in total with ',
+        #   digitalPorts, ' in use and ', totalDigitalPorts-digitalPorts, ' free')
+        # p('// and  ', totalAnalogPorts, ' analog ports in total')
 
-            # p('void setup(){\n\n}')
-            # p('void loop(){\n\n}')
+        # p('void setup(){\n\n}')
+        # p('void loop(){\n\n}')
 
 
 def addMethodsToComponents():
@@ -205,24 +202,198 @@ def addMethodsToComponents():
         # print('method', method.group)
 
 
+def addThis(component, relation):
+    for child in relations:
+        if child == relation:
+            child.fromEle = component
+
+
+def addInfoToRelations():
+    for relation in relations:
+        # print(relation.__dict__)
+        if 'test' in relation.name:
+            x = int(relation.coordinates['x'])  # + 20
+            y = int(relation.coordinates['y'])  # + 20
+            w = int(relation.coordinates['w'])  # - 60
+            h = int(relation.coordinates['h'])  # - 60
+            print(relation.name)
+
+            additional = relation.additional.split(';')
+
+            print(x, y, '   ', x+w, y, '     w:', w)
+            print(x, y+h, '   ', x+w, y+h, '     h:', h)
+            # print(additional[0])
+            # print(additional[1])
+            # print(additional[2])
+            # print(additional[3])
+            print(additional)
+
+            xF = [additional[0]]  # from and to
+            yF = [additional[1]]
+            xT = [additional[2]]
+            yT = [additional[3]]
+
+            toRight = False
+            toLeft = False
+            toBottom = False
+            toTop = False
+
+            if xF < xT:
+                toRight = True
+                print('toRight')
+            elif xF > xT:
+                toLeft = True
+                print('toLeft')
+            if yF < yT:
+                toBottom = True
+                print('toBottom')
+            elif yF > yT:
+                toTop = True
+                print('toTop')
+
+            for component in components:
+                x1 = int(component.coordinates['x'])
+                y1 = int(component.coordinates['y'])
+                x2 = int(x1)+int(component.coordinates['w'])
+                y2 = int(y1)+int(component.coordinates['h'])
+
+                if 'SimpleClass' in component.name:
+                    print(component.name)
+                    print(x1, y1, '     ', x2, y1)
+                    print(x1, y2, '     ', x2, y2)
+
+                    boundaries = [x1, x2, y1, y2]
+                    horizontal = range(x1, x2+1)
+                    vertical = range(y1, y2+1)
+                    # bond = [range(1, 4) + range(6, 99)]
+                    # if 1 in bond:
+                    #     print('in bound\n\n')
+                    # else:
+                    #     print('not in bound \n\n')
+                    # print('x', x, x2, toLeft)
+                    # print(horizontal)
+                    # print(x in horizontal)
+                    if toLeft:
+                        if x in horizontal:
+                            print('touching on horizontal')
+                        if toTop:
+                            if y in vertical:
+
+                                print('touching vertical')
+                                addThis(component, relation)
+                        elif toBottom:
+                            if y+h in vertical:
+                                addThis(component, relation)
+
+                                print('touching vertical')
+                        else:
+                            if (y+y+h)/2:
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                    elif toRight:
+                        print(x+w, horizontal)
+                        if x+w in horizontal:
+                            print('touching on horizontal')
+                        if toTop:
+                            print(y, vertical)
+                            if y in vertical:
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                        elif toBottom:
+                            if y+h in vertical:
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                        else:
+                            if((y+y+h)/2):
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                    elif toTop:
+                        # print(x, horizontal)
+                        if (x+x+w)/2 in horizontal:
+                            print('touching horizontal')
+                            print(y, vertical)
+                            if y in vertical:
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                    elif toBottom:
+                        if x in horizontal:
+                            print('touching horizontal')
+                            if y+h in vertical:
+                                print('touching vertical')
+                                addThis(component, relation)
+
+                            # print(y, y+h, vertical)
+                            # print('y+h', y+h)
+                            # if toTop and y in vertical:
+                            #     print('touching x and y on ', x, y, '\n')
+                            # elif toBottom and y+h in vertical:
+                            #     print('touching x and y2 on ', x, y+h, '\n')
+                            # if toTop and y in boundaries:
+                            #     print('touching x and y on ', x, y, '\n')
+                            # elif toBottom and y+h in boundaries:
+                            #     print('ttttouching x and y2 on ', x, y+h, '\n')
+
+                            # print('center:', (x2-x1)/2+x1, (y2-y1)/2+y1)
+                            # print('')
+
+
 def printAll():
     # for item in methods:
     #     print(item.group)
-    for item in components:
-        print(item.__dict__)
+    # for item in components:
+    #     print(item.__dict__)
         # print(item.methods)
         # print('digital', item.digitalPorts)
     #     print(item.group)
-    # # for item in relations:
-    # #     print(item.__dict__)
+    for item in relations:
+        print(item.fromEle)
 
     print(arduino.__dict__)
     # print(arduino.methods[0].__dict__)
 
 
+def generateCode():
+    file = open('./UMLet/gen.cpp', 'w')  # clear file
+    with open('./UMLet/gen.cpp', 'a') as file:
+
+        def p(*args, **kwargs):
+            print(''.join(map(str, args)), **kwargs)
+            file.write(''.join(map(str, args)), **kwargs)
+            file.write('\n')
+        file.truncate()
+
+        p('// Code generated for Arduino ', arduino.model)
+        p('// with ', arduino.digitalPorts, ' digital ports in total with ',
+          200, ' in use and ', 200, ' free')
+        p('// and  ', arduino.analogPorts, ' analog ports in total')
+        p('void setup(){\n\n}')
+        p('void loop(){\n\n}')
+
+        # print(libMethods)
+    # for port in range(0, int(component.get('digitalPorts'))):
+    #     digitalPorts += 1
+    #     p(component.get('type'), ' ', component.get('name'),
+    #       ' = ', component.get('type'), '(',  digitalPorts, ')')
+
+    # for port in range(0, int(component.get('digitalPorts'))):
+    #     digitalPorts += 1
+    #     p(component.get('type'), ' ', component.get('name'),
+    #       ' = ', component.get('type'), '(',  digitalPorts, ')')
+
+
 readUML()
 addMethodsToComponents()
 
+addInfoToRelations()
+
 
 printAll()
+
+
+# generateCode()
 print('end')
